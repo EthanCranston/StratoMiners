a0413c84946cf3ab14afb8012d30942d5b01c8f7
mzdavid08 <dm.dmeraz.dm@gmail.com>
2022-09-15 16:29:39 -0600

    Extracted darknet_ros and OpenCV installations to bash scripts to lessen docker compose time

---
+++ rosEnv/scripts/darknet.sh
@@ -0,0 +1,17 @@
+# First, create the workspace
+cd ../..
+sudo apt-get update -y
+mkdir colcon_workspace
+cd colcon_workspace
+mkdir src
+cd src
+
+# Pull all darknet images
+git clone https://github.com/leggedrobotics/darknet_ros.git -b foxy
+cd darknet_ros
+git clone https://github.com/leggedrobotics/darknet.git -b yolov3
+cd ../..
+
+# Build and source darknet
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+source install/local_setup.bash
