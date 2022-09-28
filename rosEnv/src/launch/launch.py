8c3cd2594c128d967711346dbccb04161c11ce6a
mzdavid08 <dm.dmeraz.dm@gmail.com>
2022-09-28 12:53:58 -0600

    README modified and files refactored

--- rosEnv/src/launch/launch.py
+++ rosEnv/src/launch/launch.py
@@ -1,9 +1,8 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
 
 def generate_launch_description():
 
