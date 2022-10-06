4911a41da743d227823957a0667968c115140b9f
mzdavid08 <dm.dmeraz.dm@gmail.com>
2022-10-05 23:02:51 -0600

    New darknet fork initialized as submodule in existing environment

--- rosEnv/build.sh
+++ rosEnv/build.sh
@@ -4,7 +4,7 @@
 source ./install/setup.bash
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 source /opt/ros/humble/setup.bash
-source /colcon_workspace/install/local_setup.bash
+source /rosEnv/install/local_setup.bash
 
 # Create alias for the launch file
 alias start_node="ros2 launch /rosEnv/src/launch/launch.py"
